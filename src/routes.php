34fb3bbdb6788ba679f4f5eb5de687aa061b8c00
adelharchi <adel.harchi@gmail.com>
2016-11-07 11:40:43 +0100

    Premier commit

---
+++ src/routes.php
@@ -0,0 +1,34 @@
+<?php
+// Routes
+
+$app->get('/bureau/[{lecon}]', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+    });
+
+$app->get('/[{name}]', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+
+
+
+$app->get('/install}]', function ($request, $response, $args) {
+	$this->db;
+    	$capsule = new \Illuminate\Database\Capsule\Manager;
+    	$capsule::schema()->create('articles', function (\Illuminate\Database\Schema\Blueprint $table) {
+        	$table->increments('id');
+        	$table->string('name');
+        	// Include created_at and updated_at
+        	$table->timestamps();
+    	});
+    	});
+// Delete table if needed
+// $capsule::schema()->dropIfExists('articles');
